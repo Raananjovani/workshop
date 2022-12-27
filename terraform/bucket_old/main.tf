10e976acf29b0265717eb05b20f2568a243321ce
rj185092 <69118674+rj185092@users.noreply.github.com>
2022-12-27 11:57:46 +0200

    new lab delete old

--- terraform/bucket_old/main.tf
+++
@@ -1,5 +0,0 @@
-module "s3" {
-    source = "../"
-    #bucket name should be unique
-    bucket_name = "'sdfjhghjd33'"       
-}
--- terraform/bucket_old/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-    version    = "~> 4.0"
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region     = "${var.region}"
-}
